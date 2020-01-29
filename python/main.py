511528df3ebe5db36ad464647cd0f3b7d9c1ca75
thomascbyrne12 <47927482+thomascbyrne12@users.noreply.github.com>
2020-01-28 20:57:24 -0500

    new

--- python/main.py
+++ python/main.py
@@ -10,12 +10,4 @@ total_runs = cricket_data[['Player', 'Runs']]
 average = cricket_data[['Player', 'Avg']]
 hundreds = cricket_data[['Player', '100', '50']]
 ducks = cricket_data[['Player', '0']]
-
-# Calculating the Career Length
-cricket_data['Span1']=cricket_data['Span'].str[0:4]
-cricket_data['Span2']=cricket_data['Span'].str[5:9]
-career = year2 - year1
-cricket_data['Career Length'] = career
-
-# Creating the Career Length Subset
-longest_career = cricket_data[['Player', 'Career Length']]
+longest_career = cricket_data[['Player', 'CareerLength']]
--- static/app.js
+++ static/app.js
@@ -7,20 +7,5 @@ d3.json(url, function(cricket_data) {
 });
 
 function create_bar(data) {
-    // Define SVG area dimensions
-    var svgWidth = 960;
-    var svgHeight = 660;
-
-    // Define the chart's margins as an object
-    var chartMargin = {
-        top: 30,
-        right: 30,
-        bottom: 30,
-        left: 30
-    };
-
-    // Define dimensions of the chart area
-    var chartWidth = svgWidth - chartMargin.left - chartMargin.right;
-    var chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
 
 };
